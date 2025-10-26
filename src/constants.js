9081b59a66f59f6694efff06a2c6b0ad6a7c8aa2
Ruben Chevez <rubencg195@hotmail.com>
2025-10-26 00:36:07 -0230

    Update: Correct Walking Spider project repository URL

--- src/constants.js
+++ src/constants.js
@@ -95,7 +95,7 @@ export const PROJECTS_FALLBACK = [
     id: 'walking-spider-openai-pybullet',
     name: 'RL Training for Quadruped Robot Locomotion',
     description: 'Train a simulated 8-legged spider robot to walk using reinforcement learning (PPO). Uses PyBullet for physics simulation and OpenAI Gym for the RL environment.',
-    html_url: 'https://github.com/rubencg195/WalkingSpider_OpenAI_PyBullet',
+    html_url: 'https://github.com/rubencg195/rl-training-quadruped-robot-pybullet-openai-gym',
     stargazers_count: 0,
     forks_count: 0,
     language: ['Python'],
